a522a6267ea0d652a5c5077d9e3c46f1fe3c6595
snowking29 <adrian_arellano29@hotmail.com>
2022-05-06 02:27:28 -0500

    changes applied on styles 6/05

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes} from "react-router-dom";
-import SideBar from "./components/sidebar/SideBar";
-import Content from "./components/content/Content";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Dashboard from './views/Dashboard';
+import Login from './views/Login';
+import Productos from './views/Productos';
+import Categorias from './views/Categorias';
+import Clientes from './views/Clientes';
+import Proveedores from './views/Proveedores';
+import Entradas from './views/Entradas';
+import PuntoDeVenta from './views/PuntoDeVentas';
+import PrivateRoute from './components/utils/PrivateRoute';
 import './App.css';
 
 const App =() => {
-  const [sidebarIsOpen, setSidebarOpen] = useState(true);
-  const toggleSidebar = () => setSidebarOpen(!sidebarIsOpen);
   return (
     <Router>
-      <div className="App wrapper">
-        <SideBar toggle={toggleSidebar} isOpen={sidebarIsOpen} />
-        <Content toggleSidebar={toggleSidebar} sidebarIsOpen={sidebarIsOpen} />
-      </div>
+        <Routes>
+            <Route path ="/login" element={<Login />} exact/>
+            <Route element={<PrivateRoute component={Dashboard} />}>
+                <Route path="/dashboard" element={<Dashboard />}/>
+            </Route>
+            <Route element={<PrivateRoute component={Productos} exact/>}>
+                <Route path="/productos" element={<Productos />}/>
+            </Route>
+            <Route element={<PrivateRoute component={Categorias} exact/>}>
+                <Route path="/categorias" element={<Categorias />}/>
+            </Route>
+            <Route element={<PrivateRoute component={Clientes} exact/>}>
+                <Route path="/clientes" element={<Clientes />}/>
+            </Route>
+            <Route element={<PrivateRoute component={Proveedores} exact/>}>
+                <Route path="/proveedores" element={<Proveedores />}/>
+            </Route>
+            <Route element={<PrivateRoute component={Entradas} exact/>}>
+                <Route path="/entradas" element={<Entradas />}/>
+            </Route>
+            <Route element={<PrivateRoute component={PuntoDeVenta} exact/>}>
+                <Route path="/puntodeventa" element={<PuntoDeVenta />}/>
+            </Route>
+        </Routes>
     </Router>
   );  
 }
